30e46a02e688fb2ac04605ee58d90bfb86c21648
Sagar Bharati <bharatisagar678@gmail.com>
2025-07-13 09:34:41 +0530

    feat: add Dashboard component and integrate it into routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { LoginForm } from "./components/login-form"
 import { RegisterForm } from "./components/register-form"
 import { useState } from "react";
-
+import {Dashboard} from "./components/Dashboard";
 function App() {
   const [token, setToken] = useState({ access_token: null, refresh_token: null });
 
@@ -13,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Dashboard/>}/>
         <Route path="/login" element={<LoginForm className="flex min-h-svh flex-col items-center justify-center" />} />
         <Route path="/register" element={<RegisterForm className="flex min-h-svh flex-col items-center justify-center" />} />
       </Routes>
---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+export const Dashboard = () => {
+  return (
+    <div>
+      Inbox
+    </div>
+  )
+}
+
+export default Dashboard

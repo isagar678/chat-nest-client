0be951bcd477ee1ef595f404f7538a3babf4e98a
Sagar Bharati <bharatisagar678@gmail.com>
2025-07-30 17:14:38 +0530

    removed strictt mode and added socket context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'; 
 import { AuthProvider } from "./context/AuthContext";
+import { SocketProvider } from './context/WebSocketContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+
     <AuthProvider>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </AuthProvider>
-  </StrictMode>,
+
 )

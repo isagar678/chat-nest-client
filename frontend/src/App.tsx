159f4ed8355e052fde102d49a8c2a697e0f1422f
Sagar Bharati <bharatisagar678@gmail.com>
2025-07-30 17:14:07 +0530

    route protection added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,45 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import { LoginForm } from "./components/login-form"
 import { RegisterForm } from "./components/register-form"
-import { useState } from "react";
 import {Dashboard} from "./components/Dashboard";
-function App() {
-  const [token, setToken] = useState({ access_token: null, refresh_token: null });
+import Chats from "./components/MessageBoard";
+import { useContext } from "react";
+import AuthContext from "./context/AuthContext";
 
-  if (!token) {
-    return <LoginForm setToken={setToken} />
-  }
+function App() {
+
+  const {accessToken, loading} = useContext(AuthContext)
+
+  const ProtectedRoute = ({ children }) => {
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+    
+    if (!accessToken) {
+      // Redirect to login if not authenticated
+      return <Navigate to="/login" replace />;
+    }
+    return children;
+  };
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Dashboard/>
+          </ProtectedRoute>
+        }/>
+        <Route path="/protected" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        }/>
+        <Route path="/chats" element={
+          <ProtectedRoute>
+            <Chats className="flex min-h-svh flex-col items-center justify-center"/>
+          </ProtectedRoute>
+        }/>
         <Route path="/login" element={<LoginForm className="flex min-h-svh flex-col items-center justify-center" />} />
         <Route path="/register" element={<RegisterForm className="flex min-h-svh flex-col items-center justify-center" />} />
       </Routes>
